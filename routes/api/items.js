852d4ed6b82228a576e2cf4c60faae9d90a84dbb
eugenekx <kx.eugene@gmail.com>
2020-02-26 02:09:49 +0300

    api/backend finished

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 // @route   POST api/items
-// @desc    Create A Post
+// @desc    Create Item
 // @access  Public
 router.post('/', (req, res) => {
     const newItem = new Item({
@@ -24,5 +24,15 @@ router.post('/', (req, res) => {
     newItem.save()
         .then(item => res.json(item));
 });
+
+// @route   DELETE api/items
+// @desc    Delete Item
+// @access  Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}));
+})
+
 
 module.exports = router;
